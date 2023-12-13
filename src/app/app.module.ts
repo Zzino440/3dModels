4c243e60679cc9466484d0dff95972dfdeeb45d4
Lorenzo Belli <l.belli@dstech.it>
2023-12-13 12:31:47 +0100

    added bimviewer from root project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {BimViewerComponent} from "./bim-viewer/bim-viewer.component";
+import {BimVewerComponent} from "./bim-viewer/bim-viewer.component";
 import {NgbCarouselModule} from "@ng-bootstrap/ng-bootstrap";
 import {FooterComponent} from "./footer/footer.component";
 import {TappeProgettoComponent} from './tappe-progetto/tappe-progetto.component';
@@ -12,7 +12,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BimViewerComponent,
+    BimVewerComponent,
     LaVisioneComponent,
     FooterComponent,
     TappeProgettoComponent,
--- src/app/bim-viewer/bim-viewer.component.ts
+++ src/app/bim-viewer/bim-viewer.component.ts
@@ -1,52 +1,84 @@
-import {Component, OnInit, ElementRef} from '@angular/core';
-import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
-import {Viewer, XKTLoaderPlugin} from "@xeokit/xeokit-sdk/dist/xeokit-sdk.min.es.js";
-import * as THREE from 'three';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { Location } from '@angular/common';
+import { Viewer, XKTLoaderPlugin, NavCubePlugin, TreeViewPlugin, FastNavPlugin } from "@xeokit/xeokit-sdk/dist/xeokit-sdk.min.es.js";
 
 @Component({
   selector: 'app-bim-viewer',
   templateUrl: './bim-viewer.component.html',
   styleUrls: ['./bim-viewer.component.scss']
 })
-export class BimViewerComponent implements OnInit {
+export class BimVewerComponent implements OnInit {
 
   xktLoader: any;
   modelNode: any;
-  metaModel: any;
+  viewer: any;
 
-
-  constructor(public location: Location, private element: ElementRef) {
+  constructor(public location: Location, private element : ElementRef) {
 
   }
 
   ngOnInit() {
     // xeokit library init
-    const viewer = new Viewer({
+    this.viewer = new Viewer({
       canvasId: "myCanvas",
-      cameraNear: 0.1
+      transparent: true,
+      toolbarElement: document.getElementById("myToolbar"),
+      saoEnabled: true
     });
 
-    this.xktLoader = new XKTLoaderPlugin(viewer);
-    ["./assets/bim/model.xkt"]
-      .map((path) => {
-        console.log('path: ', path)
-        this.modelNode = this.xktLoader.load({
-          id: "myCanvas",
-          // src: "./assets/bim/AC20-Institute-Var-2.xkt",
-          src: path,
-          edges: true // Emphasise edges
-        });
-      })
+    this.viewer.scene.camera.eye = [515092.9107528674, 20.706430764655682, -5037490.650574805];
+    this.viewer.scene.camera.look = [515147.3155893674, 22.564717905146725, -5037468.300825426];
+    this.viewer.scene.camera.up = [-0.02921011102805408, 0.9995012646123095, -0.01199964383315585];
+
+    new NavCubePlugin(this.viewer, {
+      canvasId: "myNavCubeCanvas",
+      visible: true,
+      size: 250,
+      alignment: "bottomRight",
+      bottomMargin: 100,
+      rightMargin: 10
+    });
+
+    new FastNavPlugin(this.viewer, {
+      hideEdges: true,
+      hideSAO: true,
+      hideColorTexture: false,
+      hidePBR: false,
+      hideTransparentObjects: false,
+      scaleCanvasResolution: false,
+      scaleCanvasResolutionFactor: 0.5,
+      delayBeforeRestore: true,
+      delayBeforeRestoreSeconds: 0.4
+    });
+
+    new TreeViewPlugin(this.viewer, {
+      containerElement: document.getElementById("treeViewContainer"),
+      hierarchy: "types",
+      autoExpandDepth: 1
+    });
+
+
+    this.xktLoader = new XKTLoaderPlugin(this.viewer);
+
+    ["./assets/bim/Architecture.xkt", "./assets/bim/Basement.xkt", "./assets/bim/Facade.xkt", "./assets/bim/MEP.xkt",
+      "./assets/bim/Structure.xkt"].map((path) => {
+      this.modelNode = this.xktLoader.load({
+        id: "myCanvas",
+        src: path,
+        saoEnabled: true,
+        edges: true,
+        dtxEnabled: true
+      });
+    })
 
     this.modelNode.on("loaded", () => {
-      const scene = viewer.scene;
-      const camera = scene.camera;
-
-      camera.eye = [-2.37, 18.97, -26.12];
-      camera.look = [10.97, 5.82, -11.22];
-      camera.up = [0.36, 0.83, 0.40];
+      this.viewer.cameraFlight.jumpTo(this.modelNode);
     });
+  }
+
+  getInfo(){
+    console.log("eye", this.viewer.scene.camera.eye);
+    console.log("look", this.viewer.scene.camera.look);
+    console.log("up", this.viewer.scene.camera.up);
   }
 }
